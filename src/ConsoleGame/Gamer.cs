e2c2c69c23eff6de2b2bbe63381726b566e6db59
Alper Konuralp <alper@konuralp.gen.tr>
2021-04-07 13:14:59 +0300

    I make a some refactorings.

--- src/ConsoleGame/Gamer.cs
+++ src/ConsoleGame/Gamer.cs
@@ -1,10 +1,11 @@
-﻿using ConsoleGame.Db;
+﻿using AutoMapper;
+using ConsoleGame.Db;
 using ConsoleGame.Entities;
+using ConsoleGame.Events;
 using ConsoleTables;
 using ConsoleTools;
-using GameAnalyticsSDK.Net;
+using Rebus.Bus;
 using System;
-using System.Security.Cryptography;
 
 namespace ConsoleGame
 {
@@ -12,139 +13,100 @@ namespace ConsoleGame
 	{
 		private readonly IDbManager _dbManager;
 		private readonly GameConfig _gameConfig;
+		private readonly IPlayGame _playGame;
+		private readonly IBus _bus;
+		private readonly IMapper _mapper;
 		private string[] _args;
 
-		public Gamer(IDbManager dbManager)
+		public Gamer(
+			IDbManager dbManager
+			, IPlayGame playGame
+			, GameConfig gameConfig
+			, IBus bus
+			, IMapper mapper)
 		{
 			_dbManager = dbManager;
-			_gameConfig = _dbManager.GetGameConfig();
+			_playGame = playGame;
+			_gameConfig = gameConfig;
+			_bus = bus;
+			_mapper = mapper;
 		}
 
 		public void Start(string[] args)
 		{
 			_args = args;
 			CheckSetup();
-			SetupGameAnalytics();
 			WriteScore();
 			WriteMenu();
-		}
-
-		private void SetupGameAnalytics()
-		{
-			// GameAnalytics.SetEnabledInfoLog(true)
-			// GameAnalytics.SetEnabledVerboseLog(true)
-
-			GameAnalytics.ConfigureBuild("0.10");
-			GameAnalytics.ConfigureUserId($"ConsoleGame_{_gameConfig.UserId}");
-
-			GameAnalytics.ConfigureAvailableResourceCurrencies("gems", "gold");
-			GameAnalytics.ConfigureAvailableResourceItemTypes("boost", "lives");
-
-			GameAnalytics.ConfigureAvailableCustomDimensions01("ninja", "samurai");
-			GameAnalytics.ConfigureAvailableCustomDimensions02("whale", "dolpin");
-			GameAnalytics.ConfigureAvailableCustomDimensions03("horde", "alliance");
-
-			GameAnalytics.Initialize("0561632006fdb5b79e1d0c05a271ce64", "49c2dd90ef213b9ce78dcb9311f97bd9b7c51bd5");
 		}
 
 		private void WriteMenu()
 		{
 			var menu = new ConsoleMenu(_args, level: 0)
-				.Add("Play Game", PlayGame)
+				.Add("Play Easy Game", () => _playGame.Play(GameLevel.Easy))
+				.Add("Play Normal Game", () => _playGame.Play(GameLevel.Normal))
+				.Add("Play Hard Game", () => _playGame.Play(GameLevel.Hard))
 				.Add("Game History", WriteHistory)
-				//.Add("Two", () => SomeAction("Two"))
-				//.Add("Three", () => SomeAction("Three"))
-				//.Add("Change me", (thisMenu) => thisMenu.CurrentItem.Name = "I am changed!")
-				//.Add("Close", ConsoleMenu.Close)
-				//.Add("Action then Close", (thisMenu) => { SomeAction("Close"); thisMenu.CloseMenu(); })
-				.Add("Exit", () => Environment.Exit(0))
+				.Add("Exit", () =>
+				{
+					Environment.Exit(0);
+				})
 				.Configure(config =>
 				{
 					config.Selector = "--> ";
 					config.EnableFilter = true;
-					config.Title = "The Console Game";
-					config.EnableWriteTitle = true;
+					config.EnableWriteTitle = false;
 					config.EnableBreadcrumb = false;
 					config.WriteHeaderAction = WriteScore;
+					config.ItemForegroundColor = ConsoleColor.DarkGray;
+					config.SelectedItemBackgroundColor = ConsoleColor.Black;
+					config.SelectedItemForegroundColor = ConsoleColor.Red;
 				});
 
 			menu.Show();
 		}
 
-		private static int Next(int min, int max)
-		{
-			return RandomNumberGenerator.GetInt32(min, max);
-		}
-
-		private void PlayGame()
-		{
-			GameAnalytics.AddProgressionEvent(EGAProgressionStatus.Start, "ConsoleGame");
-
-			PlayHistory history = new PlayHistory()
-			{
-				UserId = _gameConfig.UserId,
-				StartTime = DateTimeOffset.UtcNow.DateTime,
-			};
-
-			Console.Clear();
-			Console.WriteLine("The Console Game");
-			Console.WriteLine(new string('-', 30));
-
-			var number = Next(1, 1001);
-			int shoot = 0;
-
-			int i;
-			for (i = 1; i <= 10; i++)
-			{
-				do
-				{
-					Console.Write($"{i}. Shoot : ");
-					var a = Console.ReadLine();
-					if (int.TryParse(a, out shoot))
-					{
-						break;
-					}
-					shoot = 0;
-				} while (shoot == 0);
-
-				if (shoot == number)
-				{
-					Console.WriteLine("You win");
-					break;
-				}
-				else if (shoot < number)
-				{
-					Console.WriteLine("Make the number bigger.");
-				}
-				else
-				{
-					Console.WriteLine("Make the number smaller.");
-				}
-			}
-			history.FinishTime = DateTimeOffset.UtcNow.DateTime;
-			_gameConfig.NumberOfPlays++;
-			if (i == 11)
-			{
-				Console.WriteLine("You Lost!...");
-				history.IsTheWinner = false;
-				GameAnalytics.AddProgressionEvent(EGAProgressionStatus.Complete, "ConsoleGame", 0);
-			}
-			else
-			{
-				history.IsTheWinner = true;
-				_gameConfig.NumberOfWins++;
-				GameAnalytics.AddProgressionEvent(EGAProgressionStatus.Complete, "ConsoleGame", (11 - i) * 100);
-			}
-			_dbManager.SetGameConfig(_gameConfig);
-			_dbManager.SetPlayHistory(history);
-
-			Console.WriteLine("Please enter to the continue.");
-			Console.ReadLine();
-		}
-
 		private void WriteScore()
 		{
-			Console.WriteLine($"Score : {_gameConfig.NumberOfWins} / {_gameConfig.NumberOfPlays}");
+			ConsoleWriteWithColor(ConsoleColor.Blue, @"----------------
+The Console Game
+----------------");
+			Console.WriteLine();
+
+			ConsoleWriteWithColor(ConsoleColor.Gray, "Match Counts (Won/Total) : ");
+			ConsoleWriteWithColor(ConsoleColor.White, _gameConfig.NumberOfWins);
+			ConsoleWriteWithColor(ConsoleColor.Gray, " / ");
+			ConsoleWriteWithColor(ConsoleColor.White, _gameConfig.NumberOfPlays);
+
+			ConsoleWriteWithColor(ConsoleColor.Gray, "   Score : ");
+			ConsoleWriteWithColor(ConsoleColor.Green, _gameConfig.UserPoints);
+
+			Console.WriteLine();
+
+			ConsoleWriteWithColor(ConsoleColor.Gray, "                    Easy : ");
+			ConsoleWriteWithColor(ConsoleColor.White, _gameConfig.NumberOfEasyWins);
+			ConsoleWriteWithColor(ConsoleColor.Gray, " / ");
+			ConsoleWriteWithColor(ConsoleColor.White, _gameConfig.NumberOfEasyPlays);
+
+			ConsoleWriteWithColor(ConsoleColor.Gray, "   Normal : ");
+			ConsoleWriteWithColor(ConsoleColor.White, _gameConfig.NumberOfNormalWins);
+			ConsoleWriteWithColor(ConsoleColor.Gray, " / ");
+			ConsoleWriteWithColor(ConsoleColor.White, _gameConfig.NumberOfNormalPlays);
+
+			ConsoleWriteWithColor(ConsoleColor.Gray, "   Hard : ");
+			ConsoleWriteWithColor(ConsoleColor.White, _gameConfig.NumberOfHardWins);
+			ConsoleWriteWithColor(ConsoleColor.Gray, " / ");
+			ConsoleWriteWithColor(ConsoleColor.White, _gameConfig.NumberOfHardPlays);
+
+			Console.WriteLine();
+		}
+
+		private void ConsoleWriteWithColor(ConsoleColor color, object message)
+		{
+			var old = Console.ForegroundColor;
+			Console.ForegroundColor = color;
+			Console.Write(message);
+			Console.ForegroundColor = old;
 		}
 
 		private void WriteHistory()
@@ -154,12 +116,26 @@ namespace ConsoleGame
 			Console.WriteLine(new string('-', 30));
 
 			var history = _dbManager.ListOfLastNItem(_gameConfig.UserId, 10);
-			var table = new ConsoleTable("Date / Time", "Duration", "Status");
+			var table = new ConsoleTable(
+				"Level",
+				"Date / Time",
+				"Duration",
+				"Status",
+				"Game Point",
+				"Iteration Number",
+				"Number Held");
 			table.Options.EnableCount = false;
 
 			foreach (var item in history)
 			{
-				table.AddRow(item.FinishTime, item.FinishTime - item.StartTime, item.IsTheWinner ? "Won" : "Lost");
+				table.AddRow(
+					item.Level.ToString(),
+					item.FinishTime,
+					item.FinishTime - item.StartTime,
+					(item.IsTheWinner ?? false) ? "Won" : "Lost",
+					item.GamePoint,
+					item.IterationNumber,
+					item.PcNumber);
 			}
 
 			table.Write(Format.MarkDown);
@@ -181,7 +157,7 @@ namespace ConsoleGame
 			Console.WriteLine("Please Enter User Name : ");
 			var userName = Console.ReadLine();
 			_gameConfig.UserName = userName;
-			_dbManager.SetGameConfig(_gameConfig);
+			_bus.Send(_mapper.Map<SaveGameConfig>(_gameConfig));
 			Console.Clear();
 		}
 	}
